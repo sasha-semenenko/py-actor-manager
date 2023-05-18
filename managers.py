78f2097bb59283502706192278827997d69bb1c8
Sasha Semenenko <semenenko233249@gmail.com>
2023-05-18 12:54:29 +0300

    Solution

--- managers.py
+++ managers.py
@@ -7,17 +7,17 @@ class ActorManager:
         self._connection = sqlite3.connect("cinema.db3")
         self.table_name = "actors"
 
-    def all(self):
-        actors_data_cursor = self._connection.execute(
-            f"SELECT id, first_name, last_name FROM {self.table_name}"
-        )
-        return [Actor(*row) for row in actors_data_cursor]
-
     def create(self, first_name: str, last_name: str):
         self._connection.execute(
             f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)", (first_name, last_name,)
         )
         self._connection.commit()
+
+    def all(self):
+        actors_data_cursor = self._connection.execute(
+            f"SELECT id, first_name, last_name FROM {self.table_name}"
+        )
+        return [Actor(*row) for row in actors_data_cursor]
 
     def update(self,
                id_to_update: int,
